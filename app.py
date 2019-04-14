09eb70cc190cbb096964420cfc3b4df7c7e4dca9
gsolvit <752622384@qq.com>
2019-04-14 18:06:27 +0800

    second commit

--- app.py
+++ app.py
@@ -1,56 +1,44 @@
 # -*- coding:utf-8 -*-
-from flask import Flask, render_template, request, flash
-from flask_sqlalchemy import  SQLAlchemy
+from flask import Flask, render_template, request, flash, redirect
+import pymysql
+from model import config
 
 # 创建flask对象
 app = Flask(__name__)
-# flash 消息闪现 --> 需要加密 secret_key 加密消息的混淆
-app.secret_key = 'gsolvit'
-
-# 数据库设计
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:123456@127.0.01/studentTrainPlan"
-
-# 配置flask配置对象中键：SQLALCHEMY_COMMIT_TEARDOWN,设置为True,应用会自动在每次请求结束后提交数据库中变动
-app.config['SQLALCHEMY_COMMIT_TEARDOWN'] = True
-# 跟踪数据库修改
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
-# 获取SQLAlchemy实例对象，接下来就可以使用对象调用数据
-db = SQLAlchemy(app)
-
-# 数据库模型
-class student(db.Model):
-    # 定义表名
-    __tablename__ = 'student'
-
-    # 定义字段
-    name = db.Column(db.String(255))
 
 
-
-
-
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    url_str = 'baidu.com'
-    if request.method == 'POST':
-        # 获取用户信息
-        username = request.form.get('username')
-        password = request.form.get('password')
-        password2 = request.form.get('password2')
+    db = pymysql.connect('localhost', 'root', config['MYSQL_PASSWORD'], config['DATABASE_NAME'], charset='utf8')
+    cur = db.cursor()
+    sql = 'select * from student'
+    cur.execute(sql)
+    result = cur.fetchall()
+    print(result)
+    db.commit()
+    cur.close()
+    db.close()
+    return render_template('index.html')
 
-        # 判断填写完整
-        # flash 消息闪现 --> 需要加密 secret_key 加密消息的混淆
-        if not all([username, password, password2]):
-            flash('参数不完整')
-        elif password2 != password:
-            flash('密码不一致')
-        else:
-            return 'success'
-
-    return render_template('index.html', url_str=url_str)
 
 
 if __name__ == '__main__':
     app.run()
+
+
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'gsolvit'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+config = {
+    'default': Config,
+    'MYSQL_PASSWORD': '123456',
+    'DATABASE_NAME': 'studentTrainPlan'
+}
---
+++ model/config.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'gsolvit'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+config = {
+    'default': Config,
+    'MYSQL_PASSWORD': '123456',
+    'DATABASE_NAME': 'studentTrainPlan'
+}
