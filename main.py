c9c278a78b7710fbf259daa879c098510147c128
wpy <wangpeiyi@9979.gmail.com>
2019-05-05 00:30:17 +0800

    m

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def personal_information():
 
 @app.route('/train_plan', methods=['GET', 'POST'])
 def train_plan():
-    data = {'name': '数据转换成功', 'children': [{'name': '123123123', 'children': [{'name': 'FlareVis', 'value': 4116, 'itemStyle': {'borderColor': 'red'}}]}, {'name': 'scale', 'children': [{'name': 'TimeScale', 'value': 5833, 'categories': 1, 'itemStyle': {'borderColor': 'red'}}]}, {'name': 'display', 'children': [{'name': 'DirtySprite', 'value': 8833, 'itemStyle': {'borderColor': 'red'}}]}]}
+    data = {'name': '我的课程', 'children': [{'name': '123123123', 'children': [{'name': 'FlareVis', 'value': 4116, 'itemStyle': {'borderColor': 'red'}}]}, {'name': 'scale', 'children': [{'name': 'TimeScale', 'value': 5833, 'categories': 1, 'itemStyle': {'borderColor': 'red'}}]}, {'name': 'display', 'children': [{'name': 'DirtySprite', 'value': 8833, 'itemStyle': {'borderColor': 'red'}}]}]}
     return render_template('train_plan.html')
 
 @app.route('/get_info', methods=['GET', 'POST'])
